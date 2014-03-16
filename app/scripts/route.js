b39edb935b6007bfb3629e70509b617a1a2ee091
Andy Pliszka <andy@isp44.com>
2014-03-16 15:21:04 -0400

    feature_5_finished

--- app/scripts/route.js
+++ app/scripts/route.js
@@ -1,4 +1,8 @@
 Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+    this.route("high");
+    this.route("medium");
+    this.route("low");
+  });
 });
 
--- app/scripts/routes/todos_route.js
+++ app/scripts/routes/todos_route.js
@@ -1,6 +1,34 @@
-Todos.TodosRoute = Ember.Route.extend({
+Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('todo');
   }
 });
+
+Todos.TodosPriorityRoute = Ember.Route.extend({
+  controllerName: 'Todos',
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller});
+  },
+
+  filterByPriority: function(priority) {
+    console.log("filtere", priority);
+    return this.store.filter('todo', function(todo) {
+      return todo.get('priority') == priority;
+    });
+  }
+});
+
+Todos.TodosHighRoute = Todos.TodosPriorityRoute.extend({
+  model: function() { return this.filterByPriority('high'); },
+});
+
+Todos.TodosMediumRoute = Todos.TodosPriorityRoute.extend({
+  model: function() { console.log("medium"); return this.filterByPriority('medium'); },
+});
+
+Todos.TodosLowRoute = Todos.TodosPriorityRoute.extend({
+  model: function() { return this.filterByPriority('low'); },
+});
+
 
